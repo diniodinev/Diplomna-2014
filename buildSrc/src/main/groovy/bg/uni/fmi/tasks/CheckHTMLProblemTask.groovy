18538f7642ac164b004967faf8d50b573d8c162a
diniodinev <dinio.dinev@gmail.com>
2014-06-02 22:20:28 +0300

    Add CheckHTMLProblemTask functionallity

--- buildSrc/src/main/groovy/bg/uni/fmi/tasks/CheckHTMLProblemTask.groovy
+++ buildSrc/src/main/groovy/bg/uni/fmi/tasks/CheckHTMLProblemTask.groovy
@@ -11,48 +11,52 @@ import org.jsoup.select.Elements
 import com.rockymadden.stringmetric.similarity.JaroMetric
 import com.rockymadden.stringmetric.similarity.LevenshteinMetric
 import org.cyberneko.html.parsers.SAXParser
+import org.gradle.api.GradleException
 
 import groovy.xml.XmlUtil
 
 
 class CheckHTMLProblemTask extends DefaultTask {
-    @Input @Optional
+    @Input
+    @Optional
     String url
 
     @TaskAction
-    def checkProblems(){
-        project.extensions.problems.each{
+    def checkProblems() {
+        project.extensions.problems.each {
             boolean isSolutionFound
-            def page = new XmlSlurper(new SAXParser()).parse(url)
-            //println page.BODY[0].P[0].text()
+            def page
+            try {
+                if (it.url != null) {
+                    page = new XmlSlurper(new SAXParser()).parse(it.url)
+                } else {
+                    page = new XmlSlurper(new SAXParser()).parse(url)
+                }
+            } catch (NullPointerException ex) {
+                throw new GradleException("The 'url' property of the ${it.name} problem has to be specified. Or" +
+                        "'checkProblems' property url of checkProblems task.")
+            }
             String htmlBody = XmlUtil.serialize(page.BODY)
 
-            //If the problem has it's own URL, it takes advantage over the url defined for the extention
-            if(it.url==null) {
-                isSolutionFound = (htmlBody.indexOf(it.problemSolution)==-1)?false:true
-              println isSolutionFound
+            isSolutionFound = (htmlBody.indexOf(it.problemSolution) == -1) ? false : true
 
-//                println it.problemSolution
-
+            println isSolutionFound
 
 //                //println JaroMetric.compare(it.problemSolution,text)
 //               println LevenshteinMetric.compare("a","aaaaaaaaaaaaaaa")
 
-               //println text.contains(it.problemSolution)
-
+            //println text.contains(it.problemSolution)
 
 //                Elements answerers = document.select("#answers .user-details a");
 //                for (Element answerer : answerers) {
 //                    System.out.println("Answerer: " + answerer.text());
 //                }
-            }
+        }
 //            println "WWWWWW"
 //            println it.problemSolution
 //            println it.url
 //            println url
 //            println htmlBody
-
-        }
 
     }
 
