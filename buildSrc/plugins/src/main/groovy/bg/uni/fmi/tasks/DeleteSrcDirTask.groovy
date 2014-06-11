323843310c9cca844f08152326588e2c87928fba
diniodinev <dinio.dinev@gmail.com>
2014-06-11 15:54:21 +0300

    fix:bug

--- buildSrc/plugins/src/main/groovy/bg/uni/fmi/tasks/DeleteSrcDirTask.groovy
+++ buildSrc/plugins/src/main/groovy/bg/uni/fmi/tasks/DeleteSrcDirTask.groovy
@@ -9,8 +9,8 @@ import static bg.uni.fmi.plugins.FMIJavaPlugin.GET_INFO_TASK_NAME
 
 public class DeleteSrcDirTask extends Delete {
 
-    private def filesToDelete = []
-    private def getInfoTask = project.tasks.getByName(GET_INFO_TASK_NAME)
+    def filesToDelete = []
+    def getInfoTask = project.tasks.getByName(GET_INFO_TASK_NAME)
 
     DeleteSrcDirTask() {
         //We must configure filesToDelete in after evaluate phase because before that we do not have
